deebc10831345594df98bbbdc041442fd8e945a6
Victor Andrade <64561754+thauan11@users.noreply.github.com>
2022-12-23 11:53:00 -0300

    1.1.0
    Transformado chamadas duplicadas em funções
    Adicionado barra lateral
    Alterado estilo checkbox
    Adicionado botão Color Switch
    Centralizado conteúdo
    Ajustado alertas

--- app.js
+++ app.js
@@ -1,128 +1,109 @@
-var pagina = window.location.pathname
+let pagina = window.location.pathname
+let layoutAtual = 'black'
 
-function gerarSenha(){
-    var tamanhoSenha = document.getElementById('tamanhoNumero').value
+// GERA A SENHA
+function gerarSenha() {
+    let tamanhoSenha = document.getElementById('tamanhoNumero').value
 
     if (tamanhoSenha < 1 | tamanhoSenha > 16) {
-        if (pagina.includes('index.html')) {
-            alertaTamanho()
-        } else {
+        if (pagina.includes('popup.html')) {
             alertaTamanhoPopUp()
+            return null
+        } else {
+            alertaTamanho()
+            return null
         }
     } else {
-        //MOSTRA A DIV DA SENHA
-        mostrarSenha()
-
-        //CHECKBOX'S
         let checkBoxNum = document.getElementById('cbxNumero')
         let checkBoxCaractere = document.getElementById('cbxCaractere')
-    
-        if (checkBoxNum.checked & checkBoxCaractere.checked) { //TODAS AS OPÇÕES MARCADAS
-            if (tamanhoSenha % 2 === 0) {
-                var tamanhoLetra = (tamanhoSenha/2)-1
-                var tamanhoNumero = tamanhoSenha/2 
-            } else {
-                tamanhoSenha = tamanhoSenha-1
-                var tamanhoLetra = (tamanhoSenha/2)
-                var tamanhoNumero = tamanhoSenha/2 
+        mostrarSenha()
+        if (checkBoxNum.checked & checkBoxCaractere.checked) {// SE CARACTERE ESPECIAL E NUMEROS ESTIVER MARCADO
+            if (impoPar(tamanhoSenha) == 'par') {
+                let x = tamanhoSenha/2
+                return document.getElementById('senhaGerada').value = letraAleatoria(x-1)+numAleatorio(x)+caractereEspecial(1)
             }
-        
-            // SELECIONA CARACTERE ESPECIAL ALEATORIOS
-            var especialAleatorio = '';
-            var especial = '!@#$%&'
-            for (var i = 0; i < 1; i++) {
-                especialAleatorio += especial.charAt(Math.floor(Math.random() * especial.length));
+            else {
+                let x = (tamanhoSenha/2)-1
+                return document.getElementById('senhaGerada').value = letraAleatoria(x)+numAleatorio(x)+caractereEspecial(1)
             }
-        
-            // SELECIONA NUMEROS ALEATORIOS
-            var numeroAleatorio = '';
-            var numeros = '0123456789';
-            for (var i = 0; i < tamanhoNumero; i++) {
-                numeroAleatorio += numeros.charAt(Math.floor(Math.random() * numeros.length));
+        }
+        else if (checkBoxNum.checked) {// SE NUMEROS ESTIVER MARCADO
+            if (impoPar(tamanhoSenha) == 'par') {
+                let x = tamanhoSenha/2
+                return document.getElementById('senhaGerada').value = letraAleatoria(x)+numAleatorio(x)
             }
-        
-            // SELECIONA LETRAS ALEATORIAS
-            var letraAleatoria = '';
-            var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            for (var i = 0; i < tamanhoLetra; i++) {
-                letraAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+            else {
+                let x = (tamanhoSenha/2)
+                return document.getElementById('senhaGerada').value = letraAleatoria(x-1)+numAleatorio(x)
             }
-    
-            var senhaGerada =  letraAleatoria+numeroAleatorio+especialAleatorio
-            return document.getElementById('senhaGerada').value = senhaGerada
-        } 
-    
-        else if (checkBoxNum.checked) { //SOMENTE A OPÇÃO NUMERO MARCADA
-            if (tamanhoSenha % 2 === 0) {
-                var tamanhoLetra = (tamanhoSenha/2)
-                var tamanhoNumero = tamanhoSenha/2 
-            } else {
-                var tamanhoLetra = (tamanhoSenha/2)-1
-                var tamanhoNumero = tamanhoSenha/2 
+        }
+        else if (checkBoxCaractere.checked) {// SE CARACTERE ESPECIAL MARCADO
+            if (impoPar(tamanhoSenha) == 'par') {
+                let x = tamanhoSenha/2
+                return document.getElementById('senhaGerada').value = letraAleatoria(x)+caractereEspecial(x)
             }
-        
-            // SELECIONA NUMEROS ALEATORIOS
-            var numeroAleatorio = '';
-            var numeros = '0123456789';
-            for (var i = 0; i < tamanhoNumero; i++) {
-                numeroAleatorio += numeros.charAt(Math.floor(Math.random() * numeros.length));
+            else {
+                let x = (tamanhoSenha/2)
+                return document.getElementById('senhaGerada').value = letraAleatoria(x-1)+caractereEspecial(x)
             }
-        
-            // SELECIONA LETRAS ALEATORIAS
-            var letraAleatoria = '';
-            var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            for (var i = 0; i < tamanhoLetra; i++) {
-                letraAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-            }
-    
-            var senhaGerada =  letraAleatoria+numeroAleatorio
-            return document.getElementById('senhaGerada').value = senhaGerada
-        } 
-    
-        else if (checkBoxCaractere.checked) { //SOMENTE A OPÇÃO CARACTERE MARCADA
-            tamanhoSenha = tamanhoSenha-1
-        
-            // SELECIONA CARACTERE ESPECIAL ALEATORIOS
-            var especialAleatorio = '';
-            var especial = '!@#$%&'
-            for (var i = 0; i < 1; i++) {
-                especialAleatorio += especial.charAt(Math.floor(Math.random() * especial.length));
-            }
-        
-            // SELECIONA LETRAS ALEATORIAS
-            var letraAleatoria = '';
-            var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            for (var i = 0; i < tamanhoSenha; i++) {
-                letraAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-            }
-    
-            var senhaGerada =  especialAleatorio+letraAleatoria
-            return document.getElementById('senhaGerada').value = senhaGerada
-        } 
-    
-        else { //NENHUMA OPÇÃO MARCADA
-            // SELECIONA LETRAS ALEATORIAS
-            var letraAleatoria = '';
-            var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            for (var i = 0; i < tamanhoSenha; i++) {
-                letraAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-            }
-            return document.getElementById('senhaGerada').value = letraAleatoria
+        }
+        else { // SE NENHUM CHECKBOX ESTIVER MARCADO
+            return document.getElementById('senhaGerada').value = letraAleatoria(tamanhoSenha)
         }
     }
 }
 
-// MOSTRA A SENHA
-function mostrarSenha() {
-    document.getElementById('form0').style.height = '215px'
+// VERIFICA SE O NUMERO É PAR OU IMPAR //
+function impoPar(tamanhoSenha) {
+    if (tamanhoSenha % 2 === 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
 }
 
+// SELECIONA X CARACTERES ESPECIAIS //
+function caractereEspecial(qtd) {
+    let caractere = ''
+    let especial = '!@#$%&'
+    for (let i = 0; i < qtd; i++) {
+        caractere += especial.charAt(Math.floor(Math.random() * especial.length));
+    }
+    return caractere
+}
+
+// SELECIONA X NUMEROS //
+function numAleatorio(qtd) {
+    var numeros = ''
+    let num = '0123456789';
+    for (let i = 0; i < qtd; i++) {
+        numeros += num.charAt(Math.floor(Math.random() * num.length));
+    }
+    return numeros
+}
+
+// SELECIONA X LETRAS //
+function letraAleatoria(qtd) {
+    var letras = ''
+    let caracteres = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'; //não tem I e l
+    for (let i = 0; i < qtd; i++) {
+        letras += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    return letras
+}
+
+// AUMENTA O TAMANHO DA DIV E EXIBE A LABEL COM A SENHA //
+function mostrarSenha() {
+    document.getElementById('form0').style.height = '225px'
+}
+
+// ALERTA TAMANHO SENHA MENOR QUE 1 E MAIOR QUE 16 //
 function alertaTamanho(){
     Swal.fire({
         title: 'Digite um valor entre 1 e 16',
         icon: 'error',
-        iconColor: 'white',
-        color: 'white',
+        iconColor: '#7747F0',
+        background: '#121212',
         timer: 1500,
         timerProgressBar: true,
         showConfirmButton: false,
@@ -130,7 +111,7 @@ function alertaTamanho(){
     })
 }
 
-// BOTÃO COPIAR SENHA
+// BOTÃO COPIAR SENHA //
 function copiar() {
     document.querySelector("#senhaGerada").select();
     document.execCommand("copy");
@@ -138,8 +119,8 @@ function copiar() {
     Swal.fire({
         title: 'Senha copiada',
         icon: 'success',
-        iconColor: 'white',
-        color: 'white',
+        iconColor: '#7747F0',
+        background: '#121212',
         timer: 1500,
         timerProgressBar: true,
         showConfirmButton: false,
@@ -147,34 +128,18 @@ function copiar() {
     })
 }
 
-//FUNÇÕES PARA O HTML POPUP
+// FUNÇÕES POP-UP //
 function abrirPopUp() {
     janela = window.open('popup.html','nova_janela','width=350,height=270',titlebar=0,resizable=0)
 }
-
-function copiarPopUp() {
-    document.querySelector("#senhaGerada").select();
-    document.execCommand("copy");
-
-    Swal.fire({
-        title: 'Senha copiada',
-        icon: 'success',
-        iconColor: 'white',
-        color: 'white',
-        timer: 1000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-        heightAuto: true,
-    })
-}
-
 function alertaTamanhoPopUp(){
     Swal.fire({
         title: 'Digite um valor entre 1 e 16',
         icon: 'error',
-        iconColor: 'white',
+        iconColor: '#7747F0',
         color: 'white',
-        timer: 1000,
+        background: '#121212',
+        timer: 1500,
         timerProgressBar: true,
         showConfirmButton: false,
         heightAuto: false,
@@ -184,3 +149,34 @@ function alertaTamanhoPopUp(){
     })
 }
 
+//BOTOES BARRA LATERAL
+function aumentarBtnPopUp() {
+    document.getElementById('box4_1').style.width = '110px'
+    document.getElementById('span_box_4_1').style.width = '65px'
+}
+function diminuirBtnPopUp() {
+    document.getElementById('box4_1').style.width = '40px'
+    document.getElementById('span_box_4_1').style.width = '0px'
+}
+function aumentarBtnColorSwith() {
+    document.getElementById('box4_2').style.width = '150px'
+    document.getElementById('span_box_4_2').style.width = '100px'
+}
+function diminuirBtnColorSwith() {
+    document.getElementById('box4_2').style.width = '40px'
+    document.getElementById('span_box_4_2').style.width = '0'
+}
+
+// FUNÇÃO QUE MUDARÁ A COR DE TODO O SITE
+function colorSwitch() {
+    Swal.fire({
+        title: 'Em construção...',
+        icon: 'warning',
+        iconColor: '#7747F0',
+        background: '#121212',
+        timer: 1500,
+        timerProgressBar: true,
+        showConfirmButton: false,
+        heightAuto: false,
+    })
+}
